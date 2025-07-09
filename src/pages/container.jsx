a1425e8c6f124052ec4cf0b264b0129b2b2d5490
Mimi L <lim.mimic@gmail.com>
2025-07-09 09:33:11 +0200

    edit: styles

--- src/pages/container.jsx
+++ src/pages/container.jsx
@@ -13,7 +13,7 @@ export const Container = () => {
     const { darkMode } = useContext(DarkModeContext);
 
     return (
-        <div className={`container ${darkMode? 'dark-mode' : 'light-mode'}`}>
+        <div className={`container dot ${darkMode? 'dark-mode' : 'light-mode'}`}>
             <ClickOutside />
             <div className='pageLeft'>
                 <div className='top'>
